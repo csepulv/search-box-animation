7ce0f35e4528d14dea992702a0b8000cbce0e4c3
Christian Sepulveda <csepulv@gmail.com>
2017-01-24 17:39:44 -0800

    simple animation: transition of width on  icon click

--- src/App.js
+++ src/App.js
@@ -1,21 +1,65 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+// (Make material-ui happy)
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+import {TextField, IconButton} from 'material-ui'
+import SearchIcon from 'material-ui/svg-icons/action/search';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {expand: false};
+
+    }
+
+    handleIconClick = () => {
+        this.setState({expand: !this.state.expand});
+    };
+
+    render() {
+        //const transition = 'width 4s cubic-bezier(0.000, 0, 1.000, 1.000)';
+        const transition = 'width 0.75s cubic-bezier(0.000, 0.795, 0.000, 1.000)';
+        const styles = {
+            smallIcon: {
+                width: 30,
+                height: 30
+            },
+            small: {
+                width: 40,
+                height: 40,
+                padding: 5,
+                top: 10
+            },
+            expanded: {
+                width: 300,
+                transition: transition
+            },
+            collapsed: {
+                width: 0,
+                transition: transition
+            }
+        };
+        const textStyle = this.state.expand ? styles.expanded : styles.collapsed;
+        const divStyle = Object.assign({}, textStyle, {border:'solid 1px black'});
+        divStyle.width += styles.small.width + 5;
+
+        return (
+            <MuiThemeProvider>
+                <div style={divStyle}>
+                    <IconButton iconStyle={styles.smallIcon} style={styles.small} onClick={this.handleIconClick}>
+                        <SearchIcon />
+                    </IconButton>
+                    <TextField id='search' style={textStyle}/>
+                </div>
+            </MuiThemeProvider>
+        );
+    }
 }
 
 export default App;
